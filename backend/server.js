2a5652e7132875b7dd64c08cb23b754d89487e63
Aman Kumar <sammar4568@gmail.com>
2026-01-24 00:05:03 +0530

    add validator

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const fileRoutes = require("./routes/file.routes");
 const accountRoutes = require("./routes/account.routes");
+const { globalLimiter } = require("./middleware/rateLimit.middleware");
 
 // Middleware
 const { errorHandler } = require("./middleware/error.middleware");
@@ -17,11 +18,13 @@ const { errorHandler } = require("./middleware/error.middleware");
 
 const app = express();
 
+
 // Connect Database
 connectDB();
 
 // Global Middleware
 app.use(express.json());
+app.use(globalLimiter);
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
@@ -43,7 +46,7 @@ app.use("/api/account", accountRoutes);
 // Error Handler (ALWAYS LAST)
 app.use(errorHandler);
 
-console.log("MONGO_URI =", process.env.MONGO_URI);
+
 
 
 // Start Server
