b2e2169970909ffef063fef14b18d4160ddd8d0e
Aman Kumar <sammar4568@gmail.com>
2026-02-15 01:24:18 +0530

    fixed issue

--- Authentication_App-main/frontend/src/context/AuthProvider.jsx
+++ Authentication_App-main/frontend/src/context/AuthProvider.jsx
@@ -1,34 +1,61 @@
 import { useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
 import { setAuthToken } from "../api/axios";
+import { getProfile } from "../api/user.api";
 
 export const AuthProvider = ({ children }) => {
-  const savedToken = localStorage.getItem("token");
-
-  const [token, setToken] = useState(savedToken);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(null);
+  const [loadingUser, setLoadingUser] = useState(true);
 
   useEffect(() => {
-    if (savedToken) {
-      setAuthToken(savedToken);
-    }
-  }, [savedToken]);
+
+    const loadUser = async () => {
+      if (!token) {
+        setUser(null);
+        setLoadingUser(false);
+        return;
+      }
+
+      try {
+
+        setAuthToken(token);
+
+        const res = await getProfile();
+        setUser(res.data.data);
+        
+      } catch (err) {
+
+        localStorage.removeItem("token");
+        setToken(null);
+        setUser(null);
+        setAuthToken(null);
+      } finally {
+        setLoadingUser(false);
+      }
+    };
+
+    loadUser();
+  }, [token]);
 
   const login = (newToken) => {
     localStorage.setItem("token", newToken);
-    setAuthToken(newToken);
     setToken(newToken);
   };
 
   const logout = () => {
     localStorage.removeItem("token");
-    setAuthToken(null);
     setToken(null);
+    setUser(null);
+    setAuthToken(null);
   };
 
   return (
     <AuthContext.Provider
       value={{
         token,
+        user,
+        loadingUser,
         isAuthenticated: !!token,
         login,
         logout,
