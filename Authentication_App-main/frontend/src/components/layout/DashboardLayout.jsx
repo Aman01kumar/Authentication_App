062da0c41da497af13c3682e94416bf27887cec6
Aman Kumar <sammar4568@gmail.com>
2026-02-24 15:42:56 +0530

    added balance and dashboard layout

---
+++ Authentication_App-main/frontend/src/components/layout/DashboardLayout.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+
+      <div className="flex-1 flex flex-col">
+
+        <Navbar />
+
+        <div className="p-6 flex-1 overflow-y-auto">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
